406e7daa9c8c9f21018c281a8dae985dd965b92a
TextError <savinraul1983@yahoo.com>
2019-08-23 11:22:18 +0100

    seting up redux and redux-persist

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import './App.css';
+import './app.scss';
 
 const App = () => (
-  <div>
-    App
+  <div className='container'>
+    app
   </div>
 );
 
---
+++ client/src/redux/actions/types.js
@@ -0,0 +1,6 @@
+const ERRORS = {
+  ERROR: 'ERROR',
+  CLEAR_ERROR: 'CLEAR_ERROR'
+};
+
+export { ERRORS }
