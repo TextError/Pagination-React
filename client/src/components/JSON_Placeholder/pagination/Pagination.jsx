dd500fcba2348c4e76924e9673d5f75f034a0b87
TextError <savinraul1983@yahoo.com>
2019-08-25 20:00:52 +0100

    raw idea

--- client/src/components/JSON_Placeholder/pagination/Pagination.jsx
+++ client/src/components/JSON_Placeholder/pagination/Pagination.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Pagination = () => {
+import { connect } from 'react-redux'; 
+import { createStructuredSelector } from 'reselect';
+
+const Pagination = ({ posts }) => {
+  const { currentPage, pages} = posts;
   return (
-    <div>
-      
+    <div className="pagination">
+      <div className='true or false' onClick={() => console.log('left')}>
+        left
+      </div>
+      <div className="pages">
+        {currentPage} / {pages[pages.length-1]}
+      </div>
+      <div className='true or false' onClick={() => console.log('right')}>
+        right
+      </div>
     </div>
   )
-}
+};
 
-export default Pagination
+Pagination.propTypes = {
+
+};
+
+const mapStateToProps = state => ({
+  posts: state.posts
+})
+
+export default connect(mapStateToProps, {  })(Pagination);
--- client/src/components/JSON_Placeholder/post/Post.jsx
+++ client/src/components/JSON_Placeholder/post/Post.jsx
@@ -3,16 +3,14 @@ import PropTypes from 'prop-types';
 
 import './post.scss';
 
-const Post = ({ title, body }) => (
+const Post = ({ title }) => (
   <div className='post'>
     <p className='title'>{title}</p>
-    <p className='body'>body{console.log(body)}</p>
   </div>
   )
 
 Post.propTypes = {
-  title: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired
 }
 
 export default Post
