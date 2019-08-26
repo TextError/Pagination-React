fd82eb2587583acc5cbcae0ccda5dad02dc96028
TextError <savinraul1983@yahoo.com>
2019-08-26 17:40:35 +0100

    pagination btn

--- client/src/components/JSON_Placeholder/pagination/Pagination.jsx
+++ client/src/components/JSON_Placeholder/pagination/Pagination.jsx
@@ -4,31 +4,36 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux'; 
 import { nextPage, prevPage } from '../../../redux/actions/posts';
 import { createStructuredSelector } from 'reselect';
+import { select_posts_current_page, select_posts_pages } from '../../../redux/selectors/posts';
 
-const Pagination = ({ posts, nextPage, prevPage }) => {
-  const { currentPage, pages} = posts;
+const Pagination = ({ currentPage, pages, nextPage, prevPage }) => {
+  const prev = currentPage > 1 ? true : false;
+  const next = currentPage < pages.length ? true : false;
   return (
     <div className="pagination">
-      <div className='true or false' onClick={() => prevPage(currentPage-1)}>
-        left
-      </div>
+      <button className='btn btn-primary' disabled={!prev} onClick={() => prevPage(currentPage-1)}>
+      <i className="fas fa-arrow-left"></i>
+      </button>
       <div className="pages">
         {currentPage} / {pages[pages.length-1]}
       </div>
-      <div className='true or false' onClick={() => nextPage(currentPage+1)}>
-        right
-      </div>
+      <button className='btn btn-primary' disabled={!next} onClick={() => nextPage(currentPage+1)}>
+        <i className="fas fa-arrow-right"></i>
+      </button>
     </div>
   )
 };
 
 Pagination.propTypes = {
+  currentPage: PropTypes.number.isRequired,
+  pages: PropTypes.array.isRequired,
   nextPage: PropTypes.func.isRequired,
   prevPage: PropTypes.func.isRequired
 };
 
-const mapStateToProps = state => ({
-  posts: state.posts
-})
+const mapStateToProps = createStructuredSelector({
+  currentPage: select_posts_current_page,
+  pages: select_posts_pages
+});
 
 export default connect(mapStateToProps, { nextPage, prevPage })(Pagination);
--- client/src/redux/selectors/posts.js
+++ client/src/redux/selectors/posts.js
@@ -13,7 +13,11 @@ export const select_posts_posts = createSelector(
   posts => posts.posts
 );
 
+export const select_posts_current_page = createSelector(
+  [select_posts_state],
+  posts => posts.currentPage
+);
 export const select_posts_pages = createSelector(
   [select_posts_state],
   posts => posts.pages
-) 
+);
