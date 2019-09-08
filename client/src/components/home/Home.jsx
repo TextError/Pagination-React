de1b7a4a11d47a20cf940f00779f870b56e4938f
TextError <savinraul1983@yahoo.com>
2019-09-08 19:49:35 +0100

    more css responsive

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -8,13 +8,15 @@ import './home.scss';
 const Home = () => {
   return (
     <div className='home'>
-      <Link to='json'>
-        <CustomButton value='Json Placeholder' isClass='' />
-      </Link>
-      <br />
-      <Link to='the-movie-db'>
-        <CustomButton value='The Movie DB' isClass='' />
-      </Link>
+      <div className='links'>
+        <Link to='json'>
+          <CustomButton value='Json Placeholder' isClass='' />
+        </Link>
+        <br />
+        <Link to='the-movie-db'>
+          <CustomButton value='The Movie DB' isClass='' />
+        </Link>
+      </div>
     </div>
   )
 }
