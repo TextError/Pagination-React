d3e7cf7755e81f91ce2dcfe798d3ef022d9f4865
TextError <savinraul1983@yahoo.com>
2019-09-08 15:54:42 +0100

    change pagination code

--- client/src/components/The_Movie_DB/pagination/Pagination.jsx
+++ client/src/components/The_Movie_DB/pagination/Pagination.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import clasnames from 'classnames';
 
 import { connect } from 'react-redux';
 import { nextPage } from '../../../redux/actions/movies';
 
+import './pagination.scss';
+
 const Pagination = ({ movies: { total_pages, page }, nextPage, movie }) => {
-  const page_element = []
+  const page_element = [];
+  
   for (let i = 1; i <= total_pages; i++) {
     let active = page === i ? 'btn btn-primary' : 'btn';
     page_element.push(
@@ -21,11 +25,15 @@ const Pagination = ({ movies: { total_pages, page }, nextPage, movie }) => {
       <div className='row'>
         <div className='col'>
           <div className='pagination'>
-            <ul>
-              {page > 1 ? <li className='btn mr-1' onClick={() => nextPage({ movie, page: page-1 })}>prev</li>: null}
-              {page_element}
-              {page < total_pages ? <li className='btn ml-1'>next</li>: null}
-            </ul>
+            <span 
+              className={clasnames('previous', {'d-none' : page <= 1})} 
+              onClick={() => nextPage({ movie, page: page-1 })}
+            >Previous</span>
+            <ul>{page_element}</ul>
+            <span 
+              className={clasnames('next', {'d-none' : page >= total_pages})}
+              onClick={() => nextPage({ movie, page: page+1 })}
+            >Next</span>
           </div>
         </div>
       </div>
