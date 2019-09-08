5f95f3862615043b2e26c0117f5c8786f4c389a9
TextError <savinraul1983@yahoo.com>
2019-09-08 17:41:20 +0100

    react-pagination npm

--- client/src/components/The_Movie_DB/pagination/Pagination.jsx
+++ client/src/components/The_Movie_DB/pagination/Pagination.jsx
@@ -1,42 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clasnames from 'classnames';
+import ReactPaginate from 'react-paginate';
 
 import { connect } from 'react-redux';
 import { nextPage } from '../../../redux/actions/movies';
 
 import './pagination.scss';
 
-const Pagination = ({ movies: { total_pages, page }, nextPage, movie }) => {
-  const page_element = [];
-  
-  for (let i = 1; i <= total_pages; i++) {
-    let active = page === i ? 'btn btn-primary' : 'btn';
-    page_element.push(
-      <li 
-        className={`mr-1 ml-1 ${active}`}
-        key={i}
-        onClick={() => nextPage({ movie, page:i })}      
-      >{i}</li>
-    )
-  }
+const Pagination = ({ movies: { total_pages }, nextPage, movie }) => {
+
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col'>
-          <div className='pagination'>
-            <span 
-              className={clasnames('previous', {'d-none' : page <= 1})} 
-              onClick={() => nextPage({ movie, page: page-1 })}
-            >Previous</span>
-            <ul>{page_element}</ul>
-            <span 
-              className={clasnames('next', {'d-none' : page >= total_pages})}
-              onClick={() => nextPage({ movie, page: page+1 })}
-            >Next</span>
-          </div>
-        </div>
-      </div>
+    <div className="pagination">
+      <ReactPaginate
+        previousLabel={'previous'}
+        nextLabel={'next'}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={total_pages}
+        marginPagesDisplayed={1}
+        pageRangeDisplayed={5}
+        onPageChange={({ selected }) => nextPage({ movie, page:selected })}
+        containerClassName={'pagination'}
+        subContainerClassName={'pages pagination'}
+        activeClassName={'active'}
+      />
     </div>
   )
 }
