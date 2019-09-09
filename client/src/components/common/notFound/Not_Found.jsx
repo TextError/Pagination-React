55c612cc4750f0bdd8d39a23565ae35cf5ace6d5
TextError <savinraul1983@yahoo.com>
2019-09-09 15:28:43 +0100

    not found quick css

--- client/src/components/common/notFound/Not_Found.jsx
+++ client/src/components/common/notFound/Not_Found.jsx
@@ -8,11 +8,11 @@ class NotFound extends Component {
   render() {
     return (
       <div className='not-found'>
-        <div className='row no-gutters'>
-          <div className='col'>
-            <div className='wrapper'>
+        <div className='wrapper'>
+          <div className='row no-gutters'>
+            <div className='col'>
               <h1 className='m-0'>Oops!</h1> 
-              <h4 className='text-center'>404 - Page not found</h4>
+              <h3>404 - Page not found</h3>
               <p>The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
               <NavLink to='/' className='nav-link text-center' onClick={this.onClick}>
                 <span className='btn btn-primary'>Go to homepage</span>
